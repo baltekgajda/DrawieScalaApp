db9b028f3182ff734d1935f21235b71e6431ace7
baltekg <bartlomiej.gajda2@gmail.com>
2018-06-08 13:16:56 +0200

    Buttons unit tests

--- src/test/scala/drawie/MainMenuViewTest.scala
+++ src/test/scala/drawie/MainMenuViewTest.scala
@@ -9,12 +9,25 @@ class MainMenuViewTest extends  FlatSpec with BeforeAndAfter{
     val jfxPanel = new JFXPanel
     val mainMenuView = MainMenuView(200,200)
 
-
   "newRoomButton" should "be initialized properly" in {
     assert(mainMenuView.newRoomButton.getLayoutX == 344.0)
     assert(mainMenuView.newRoomButton.getLayoutY == 300.0)
     assert(mainMenuView.newRoomButton.getPrefWidth == 108.0)
     assert(mainMenuView.newRoomButton.getStyleClass == FXCollections.observableArrayList("custom-button"))
+  }
+
+  "joinRoomButton" should "be initialized properly" in {
+    assert(mainMenuView.joinRoomButton.getLayoutX == 344.0)
+    assert(mainMenuView.joinRoomButton.getLayoutY == 337.0)
+    assert(mainMenuView.joinRoomButton.getPrefWidth == 108.0)
+    assert(mainMenuView.joinRoomButton.getStyleClass == FXCollections.observableArrayList("custom-button"))
+  }
+
+  "exitButton" should "be initialized properly" in {
+    assert(mainMenuView.exitButton.getLayoutX == 344.0)
+    assert(mainMenuView.exitButton.getLayoutY == 410.0)
+    assert(mainMenuView.exitButton.getPrefWidth == 108.0)
+    assert(mainMenuView.exitButton.getStyleClass == FXCollections.observableArrayList("custom-button"))
   }
 
 
