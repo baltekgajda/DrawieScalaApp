aecb639e5c7017d18a7aef2df00ccde035376d46
baltekg <bartlomiej.gajda2@gmail.com>
2018-06-08 16:03:23 +0200

    Unit testing (#8)
    * Controllers tests
    
    * remove MockFactroy

--- src/test/scala/drawie/MainMenuControllerTest.scala
+++ src/test/scala/drawie/MainMenuControllerTest.scala
@@ -1,5 +1,25 @@
 package drawie
 
-class MainMenuControllerTest {
+import javafx.embed.swing.JFXPanel
+import org.scalatest.FlatSpec
 
+class MainMenuControllerTest extends FlatSpec {
+
+  val jfxPanel = new JFXPanel
+  val mainMenuView = MainMenuView(200,200)
+
+  val roomController = MainMenuController(mainMenuView);
+
+
+  "mainMenuController" should "initialize joinRoomButton listener properly" in {
+    assert(mainMenuView.joinRoomButton.onMouseClicked.isNotNull.getValue == true)
+  }
+
+  "mainMenuController" should "initialize newRoomButton listener properly" in {
+    assert(mainMenuView.newRoomButton.onMouseClicked.isNotNull.getValue == true)
+  }
+
+  "mainMenuController" should "initialize exitButton listener properly" in {
+    assert(mainMenuView.exitButton.onMouseClicked.isNotNull.getValue == true)
+  }
 }
