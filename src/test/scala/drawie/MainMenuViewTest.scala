87054415cc31a9889197a3be453c21c376630e38
baltekg <bartlomiej.gajda2@gmail.com>
2018-06-08 13:38:18 +0200

    Added MainMenuView unit tests

--- src/test/scala/drawie/MainMenuViewTest.scala
+++ src/test/scala/drawie/MainMenuViewTest.scala
@@ -30,6 +30,39 @@ class MainMenuViewTest extends  FlatSpec with BeforeAndAfter{
     assert(mainMenuView.exitButton.getStyleClass == FXCollections.observableArrayList("custom-button"))
   }
 
+  "urlTextField" should "be initialized properly" in {
+    assert(mainMenuView.urlTextField.getLayoutX == 216.0)
+    assert(mainMenuView.urlTextField.getLayoutY == 374.0)
+    assert(mainMenuView.urlTextField.getPrefWidth == 365.0)
+    assert(mainMenuView.urlTextField.getPrefHeight == 25.0)
+    assert(mainMenuView.urlTextField.getPromptText == "type url of existing drawie room")
+    assert(mainMenuView.urlTextField.getStyleClass == FXCollections.observableArrayList("text-field"))
+  }
 
+  "titleText" should "be initialized properly" in {
+    assert(mainMenuView.titleText.getLayoutX == 257.0)
+    assert(mainMenuView.titleText.getLayoutY == 250.0)
+    assert(mainMenuView.titleText.getText == "DRAWIE")
+    assert(mainMenuView.titleText.getStyleClass == FXCollections.observableArrayList("drawie-title"))
+  }
+
+  "scenePane" should "be initialized properly" in {
+    assert(mainMenuView.scenePane.getPrefHeight == mainMenuView.sceneHeight)
+    assert(mainMenuView.scenePane.getPrefWidth == mainMenuView.sceneWidth)
+    assert(mainMenuView.scenePane.getStyleClass == FXCollections.observableArrayList("menu-background"))
+    assert(mainMenuView.scenePane.children.size() == 5)
+  }
+
+  "changeURLTextFieldPromptText" should "change prompt text to given" in {
+    val text = "TEST"
+    mainMenuView.changeURLTextFieldPromptText(text)
+    assert(mainMenuView.urlTextField.getPromptText == text)
+  }
+
+  "getURLTextFieldText" should "return typed text" in {
+    val text = "typed text"
+    mainMenuView.urlTextField.text = text
+    assert(mainMenuView.getURLTextFieldText == text)
+  }
 }
 
