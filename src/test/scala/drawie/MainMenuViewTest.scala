64d38d7377049747f6f69d93ea778b08ce4ce814
baltekg <bartlomiej.gajda2@gmail.com>
2018-06-08 13:14:03 +0200

    Minor fix in tests

--- src/test/scala/drawie/MainMenuViewTest.scala
+++ src/test/scala/drawie/MainMenuViewTest.scala
@@ -2,18 +2,21 @@ package drawie
 
 import javafx.collections.FXCollections
 import javafx.embed.swing.JFXPanel
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfter, FlatSpec}
 
-class MainMenuViewTest extends  FlatSpec {
-
-  "Test" should "do thsnck" in {
+class MainMenuViewTest extends  FlatSpec with BeforeAndAfter{
 
     val jfxPanel = new JFXPanel
     val mainMenuView = MainMenuView(200,200)
+
+
+  "newRoomButton" should "be initialized properly" in {
     assert(mainMenuView.newRoomButton.getLayoutX == 344.0)
     assert(mainMenuView.newRoomButton.getLayoutY == 300.0)
     assert(mainMenuView.newRoomButton.getPrefWidth == 108.0)
     assert(mainMenuView.newRoomButton.getStyleClass == FXCollections.observableArrayList("custom-button"))
   }
+
+
 }
 
